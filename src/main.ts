c214cad4b1e810104788112c5deb686a5e154160
keven-correa <kturvi01@gmail.com>
2022-06-11 00:02:52 -0400

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder()
-  .setTitle('Projects Management')
-  .setDescription('Clone of Jira xd...!')
-  .setVersion('1.0')
-  .addTag('projects')
-  .build();
-const document = SwaggerModule.createDocument(app, config);
-SwaggerModule.setup('api', app, document);
   app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('Projects Management')
+    .setDescription('Clone of Jira xd...!')
+    .setVersion('1.0')
+    .addTag('projects')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
